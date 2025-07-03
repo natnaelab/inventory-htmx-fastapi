e65ef380bdfbfb233b63c2b6d1ba444797960335
Yanis Gram <yfsafer@gmail.com>
2025-07-03 12:00:19 +0200

    get docker-compose running

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://appuser:apppass@192.168.200.99:3306/geraete_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://appuser:apppass@localhost:3306/geraete_db")
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)  # pool_pre_ping for connection health
 
